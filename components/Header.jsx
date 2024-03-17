ab3f7698b4197544e20b2593a43249e632fc91b7
yabokar1 <yabokar@gmail.com>
2024-03-17 12:51:59 -0400

    Add menu and profile image

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
           src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
@@ -31,7 +33,15 @@ export default function Header() {
         />
       </div>
 
-      <h1>Right sides</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className=" hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200" />
+        <img
+          src="https://avatars.githubusercontent.com/u/71052435?v=4"
+          alt="user-image"
+          className="h-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
