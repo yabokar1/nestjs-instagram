4ea0c6cee111be04dea295859048f7c180491287
yabokar1 <yabokar@gmail.com>
2024-03-17 16:51:31 -0400

    Make the feed section responsive

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,13 +2,13 @@ import Stories from "./Stories";
 import Posts from "./Posts";
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3">
+      <section className="md:col-span-2 md:max-w-6xl mx-auto">
         <Stories />
         <Posts />
       </section>
 
-      <section>
+      <section className="hidden md:inline-grid md:col-span-1">
         {/* Mini Profile */}
 
         {/* Suggestion */}
--- components/Post.jsx
+++ components/Post.jsx
@@ -3,6 +3,7 @@ import {
   HeartIcon,
   ChatIcon,
   BookmarkIcon,
+  EmojiHappyIcon,
 } from "@heroicons/react/outline";
 
 export default function Post({ img, userImg, caption, username, id }) {
@@ -27,6 +28,21 @@ export default function Post({ img, userImg, caption, username, id }) {
         </div>
         <BookmarkIcon className="btn" />
       </div>
+
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          className="border-none flex-1 focus:ring-0"
+          type="text"
+          placeholder="Enter your comment..."
+        />
+        <button className="text-blue-400 font-bold">Post</button>
+      </form>
     </div>
   );
 }
