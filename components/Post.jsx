875703faf212cfcd6ed8326b79946c627a6bc1b5
yabokar1 <yabokar@gmail.com>
2024-03-17 16:06:45 -0400

    Create header and image sections of post component

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,7 +1,19 @@
+import { DotsHorizontalIcon } from "@heroicons/react/outline";
+
 export default function Post({ img, userImg, caption, username, id }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      <img className="object-cover w-full" src={img} alt="" />
     </div>
   );
 }
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -5,14 +5,16 @@ export default function Posts() {
     {
       id: "1",
       username: "codewithsahand",
-      userImg: "https://avatars.githubusercontent.com/u/71052435?v=4",
+      userImg:
+        "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
       img: "https://images.unsplash.com/photo-1682686580452-37f1892ee5e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D",
       caption: "Nice picture",
     },
     {
       id: "2",
       username: "ghavidelsahana",
-      userImg: "https://avatars.githubusercontent.com/u/71052435?v=4",
+      userImg:
+        "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
       img: "https://images.unsplash.com/photo-1710438399422-2fca27686bcd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
       caption: "New picture from my city",
     },
