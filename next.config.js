4b49ec698e3dbd64906fe10bc6ce0b8de58e9f07
yabokar1 <yabokar@gmail.com>
2024-03-17 12:17:26 -0400

    Add instagram logo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: ["www.jennexplores.com", "upload.wikimedia.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
