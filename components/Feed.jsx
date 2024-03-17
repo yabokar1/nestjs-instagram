a347bbbd53019eb8ccdd4511451570f053059560
yabokar1 <yabokar@gmail.com>
2024-03-17 17:06:51 -0400

    Create the mini profile component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,6 @@
 import Stories from "./Stories";
 import Posts from "./Posts";
+import MiniProfile from "./MiniProfile";
 export default function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-3">
@@ -9,7 +10,9 @@ export default function Feed() {
       </section>
 
       <section className="hidden md:inline-grid md:col-span-1">
-        {/* Mini Profile */}
+        <div className="fixed w-[380px]">
+          <MiniProfile />
+        </div>
 
         {/* Suggestion */}
       </section>
