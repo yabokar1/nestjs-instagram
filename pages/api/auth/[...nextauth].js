b60a1cc7f75e40e0c05e85306abeb887dc0fa151
yabokar1 <yabokar@gmail.com>
2024-03-17 20:12:48 -0400

    Complete signin page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
+
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -9,5 +10,19 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+  secret: process.env.SECRET,
+  pages: {
+    signIn: "/auth/signin",
+  },
+
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+});
