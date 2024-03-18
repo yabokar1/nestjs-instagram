7ec63181d76f7926c6d702d43f6e6ef05652b9c2
yabokar1 <yabokar@gmail.com>
2024-03-18 12:42:32 -0400

    Modify the mini profile and story components to include the session

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -2,9 +2,11 @@ import Story from "./Story";
 import minifaker from "minifaker";
 import "minifaker/locales/en";
 import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Stories() {
   const [storyUsers, setStoryUsers] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const storyUsers = minifaker.array(20, (i) => ({
@@ -18,6 +20,13 @@ export default function Stories() {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
+      {session && (
+        <Story
+          img={session.user.image}
+          username={session.user.username}
+          isUser="true"
+        />
+      )}
       {storyUsers.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
