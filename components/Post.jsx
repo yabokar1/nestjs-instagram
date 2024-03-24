151b8b121e738fff5e10f9640d0c2e853d80ddd7
yabokar1 <yabokar@gmail.com>
2024-03-24 11:18:48 -0400

    Show comments in the comments section

--- components/Post.jsx
+++ components/Post.jsx
@@ -5,13 +5,34 @@ import {
   BookmarkIcon,
   EmojiHappyIcon,
 } from "@heroicons/react/outline";
+import Moment from "react-moment";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import { db } from "../firebase";
 export default function Post({ img, userImg, caption, username, id }) {
   const { data: session } = useSession();
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
 
   async function sendComment(event) {
     event.preventDefault();
@@ -53,6 +74,21 @@ export default function Post({ img, userImg, caption, username, id }) {
         <span className="font-bold mr-2">{username}</span>
         {caption}
       </p>
+      {comments.length > 0 && (
+        <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+          {comments.map((comment) => (
+            <div className="flex items-center space-x-2 mb-2">
+              <img
+                className="h-7 rounded-full object-cover"
+                src={comment.data().useImage}
+              />
+              <p className="font-semibold">{comment.data().username}</p>
+              <p className="flex-1 truncate">{comment.data().comment}</p>
+              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
+        </div>
+      )}
       {session && (
         <form className="flex items-center p-4">
           <EmojiHappyIcon className="h-7" />
